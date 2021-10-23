1b2fe08789fb6dfbe6a7df2e64940405fc31d02b
Rafal Gwozdzinski <rafal.gwozdzinski@gmail.com>
2021-10-23 08:31:38 +0200

    [PLTS-53] Remove no longer maintained projects

--- uniswap-json-api/default.nix
+++
@@ -1,7 +0,0 @@
-{ ... }:
-let
-  sources = import ../vendor/plutus/nix/sources.nix { inherit pkgs; };
-  pkgs = import sources.nixpkgs { };
-  drv = pkgs.haskellPackages.callCabal2nix "uniswap-json-api" ./. { };
-in
-drv
