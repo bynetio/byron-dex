704c9473db34b46e1dbd3107f4288eda9f4e2021
Jonatan Borkowski <jonatan.borkowski@pm.me>
2021-11-06 21:28:41 +0100

    Add pab url to config

--- middleware/src/Middleware/Capability/Config.hs
+++ middleware/src/Middleware/Capability/Config.hs
@@ -3,18 +3,22 @@
 module Middleware.Capability.Config where
 
 import           Conferer
+import qualified Conferer.FromConfig         as Internal
 import           Conferer.FromConfig.Warp    ()
 import qualified Conferer.Source.CLIArgs     as Cli
 import qualified Conferer.Source.Dhall       as Dhall
 import qualified Conferer.Source.Env         as Env
+import           Data.Text                   (unpack)
 import           GHC.Generics
 import           Middleware.Capability.Error
 import qualified Network.Wai.Handler.Warp    as Warp
 import           Polysemy
 import qualified Polysemy.Internal           as P
+import           Servant.Client              (BaseUrl (BaseUrl), Scheme (Http), parseBaseUrl)
 
-newtype AppConfig = AppConfig
+data AppConfig = AppConfig
   { appConfigServer :: Warp.Settings
+  , pabUrl :: BaseUrl
   } deriving (Generic)
 
 instance Conferer.FromConfig AppConfig
@@ -22,7 +26,14 @@ instance Conferer.FromConfig AppConfig
 instance DefaultConfig AppConfig where
   configDef = AppConfig
     { appConfigServer = Warp.setPort 8080 configDef
+    , pabUrl = configDef
     }
+
+instance Conferer.FromConfig BaseUrl where
+  fromConfig = Internal.fetchFromConfigWith (parseBaseUrl . unpack)
+
+instance Conferer.DefaultConfig BaseUrl where
+  configDef = BaseUrl Http "localhost" 9080 ""
 
 data ConfigLoader m a where
   Load :: ConfigLoader m AppConfig
