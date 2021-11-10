9df40b78146505099e2b7c53ca8666c35b3539c2
Rafal Gwozdzinski <rafal.gwozdzinski@gmail.com>
2021-11-10 13:41:34 +0100

    Add 'myOrders' Dex endpoint

--- middleware/src/Middleware/API.hs
+++ middleware/src/Middleware/API.hs
@@ -1,7 +1,8 @@
 module Middleware.API where
 
-import Middleware.Dex.Types       (FundView (FundView))
-import Middleware.PabClient.Types (ContractInstanceId)
-import Servant                    (Capture, Get, JSON, type (:>))
+import           Middleware.Dex.Types       (DexOrder (..), FundView (FundView))
+import           Middleware.PabClient.Types (ContractInstanceId)
+import           Servant                    (Capture, Get, JSON, (:<|>), (:>))
 
 type API = Capture "contract-instance-id" ContractInstanceId :> "funds" :> Get '[JSON] [FundView]
+      :<|> Capture "contract-instance-id" ContractInstanceId :> "myOrders" :> Get '[JSON] [DexOrder]
