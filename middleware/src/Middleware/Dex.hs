5b1591fda107b136c5c64b838bdabd8a0c36ea95
Tomasz Hołubowicz <tomasz.holubowicz@binarapps.com>
2021-11-12 08:56:41 +0000

    [PLTS-54] add endpoint for creating sell order

--- middleware/src/Middleware/Dex.hs
+++ middleware/src/Middleware/Dex.hs
@@ -8,7 +8,7 @@ import           Middleware.API                    (API)
 import           Middleware.Capability.Error
 import           Middleware.Dex.Types              hiding (Error)
 import           Middleware.PabClient              (ManagePabClient,
-                                                    cancelOrder,
+                                                    cancelOrder, createSellOrder,
                                                     createLiquidityPoolInPab,
                                                     getFunds, getMyOrders)
 import           Middleware.PabClient.Types        hiding (Error)
@@ -19,6 +19,7 @@ import           Servant.Server                    (ServerT)
 
 data Dex r a where
   Funds               :: ContractInstanceId -> Dex r [FundView]
+  CreateSellOrderEU   :: ContractInstanceId -> CreateSellOrderParams -> Dex r ()
   CreateLiquidityPool :: ContractInstanceId -> CreateLiquidityPoolParams -> Dex r ()
   MyOrders            :: ContractInstanceId -> Dex r [DexOrder]
   Cancel              :: ContractInstanceId -> MidCancelOrder -> Dex r ()
@@ -28,21 +29,25 @@ makeSem ''Dex
 runDex :: (WithLog r, Members '[ManagePabClient] r)
        => Sem (Dex ': r) a
        -> Sem r a
-runDex = interpret $
-  \case
+runDex = interpret
+  (\case
       Funds cid -> do
         fs <- getFunds cid
         pure $ fmap (uncurry fundView) fs
+      CreateSellOrderEU cid params -> do
+        createSellOrder cid params
+      CreateLiquidityPool cid params -> do
+        createLiquidityPoolInPab cid params
       MyOrders cid -> do
         os <- getMyOrders cid
         pure $ fmap dexOrder os
-      CreateLiquidityPool cid params -> do
-        createLiquidityPoolInPab cid params
       Cancel cid params -> do
         cancelOrder cid params
+  )
 
 dexServer :: Members '[Error AppError, Dex] r => ServerT API (Sem r)
 dexServer = funds
-       :<|> createLiquidityPool
-       :<|> myOrders
-       :<|> cancel
+  :<|> createSellOrderEU
+  :<|> createLiquidityPool  
+  :<|> myOrders
+  :<|> cancel
