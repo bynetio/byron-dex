fe682cfb3a31cb4fe9d9e80955152d63ab3f8fd9
Rafal Gwozdzinski <rafal.gwozdzinski@gmail.com>
2021-11-14 07:58:29 +0100

    [PLTS-87] Add middleware endpoint descriptions

--- middleware/src/Middleware/API.hs
+++ middleware/src/Middleware/API.hs
@@ -9,18 +9,42 @@ import           Middleware.Dex.Types       (CancelOrderParams (CancelOrderParam
                                              PayoutView (PayoutView),
                                              PerformRandomParams (PerformRandomParams))
 import           Middleware.PabClient.Types (ContractInstanceId)
-import           Servant                    (Capture, Get, JSON, Post, ReqBody,
-                                             (:<|>), (:>))
+import           Servant                    (Capture, Description, Get, JSON,
+                                             Post, ReqBody, (:<|>), (:>))
 
-type API = Capture "contract-instance-id" ContractInstanceId :> "funds"                                                                :> Get  '[JSON] [FundView]
-      :<|> Capture "contract-instance-id" ContractInstanceId :> "collect-funds"                                                        :> Post '[JSON] ()
-      :<|> Capture "contract-instance-id" ContractInstanceId :> "create-sell-order"      :> ReqBody '[JSON] CreateSellOrderParams      :> Post '[JSON] ()
-      :<|> Capture "contract-instance-id" ContractInstanceId :> "create-liquidity-pool"  :> ReqBody '[JSON] CreateLiquidityPoolParams  :> Post '[JSON] ()
-      :<|> Capture "contract-instance-id" ContractInstanceId :> "create-liquidity-order" :> ReqBody '[JSON] CreateLiquidityOrderParams :> Post '[JSON] ()
-      :<|> Capture "contract-instance-id" ContractInstanceId :> "orders"                                                               :> Get  '[JSON] [OrderView]
-      :<|> Capture "contract-instance-id" ContractInstanceId :> "all-orders"                                                           :> Get  '[JSON] [OrderView]
-      :<|> Capture "contract-instance-id" ContractInstanceId :> "payouts"                                                              :> Get  '[JSON] [PayoutView]
-      :<|> Capture "contract-instance-id" ContractInstanceId :> "perform"                                                              :> Post '[JSON] ()
-      :<|> Capture "contract-instance-id" ContractInstanceId :> "perform-random"         :> ReqBody '[JSON] PerformRandomParams        :> Post '[JSON] ()
-      :<|> Capture "contract-instance-id" ContractInstanceId :> "stop"                                                                 :> Post '[JSON] ()
-      :<|> Capture "contract-instance-id" ContractInstanceId :> "cancel"                 :> ReqBody '[JSON] CancelOrderParams          :> Post '[JSON] ()
+type API = Capture "contract-instance-id" ContractInstanceId :> "funds"
+           :> Description "List of user funds."
+           :> Get '[JSON] [FundView]
+      :<|> Capture "contract-instance-id" ContractInstanceId :> "collect-funds"
+           :> Description "Collect user funds."
+           :> Post '[JSON] ()
+      :<|> Capture "contract-instance-id" ContractInstanceId :> "create-sell-order"
+           :> Description "Create a sell order."
+           :> ReqBody '[JSON] CreateSellOrderParams :> Post '[JSON] ()
+      :<|> Capture "contract-instance-id" ContractInstanceId :> "create-liquidity-pool"
+           :> Description "Create a liquidity pool"
+           :> ReqBody '[JSON] CreateLiquidityPoolParams :> Post '[JSON] ()
+      :<|> Capture "contract-instance-id" ContractInstanceId :> "create-liquidity-order"
+           :> Description "Create a liquidity order."
+           :> ReqBody '[JSON] CreateLiquidityOrderParams :> Post '[JSON] ()
+      :<|> Capture "contract-instance-id" ContractInstanceId :> "orders"
+           :> Description "List user orders."
+           :> Get '[JSON] [OrderView]
+      :<|> Capture "contract-instance-id" ContractInstanceId :> "all-orders"
+           :> Description "List all orders."
+           :> Get '[JSON] [OrderView]
+      :<|> Capture "contract-instance-id" ContractInstanceId :> "payouts"
+           :> Description "List user payouts."
+           :> Get '[JSON] [PayoutView]
+      :<|> Capture "contract-instance-id" ContractInstanceId :> "perform"
+           :> Description "Perform match."
+           :> Post '[JSON] ()
+      :<|> Capture "contract-instance-id" ContractInstanceId :> "perform-random"
+           :> Description "Perform random match."
+           :> ReqBody '[JSON] PerformRandomParams  :> Post '[JSON] ()
+      :<|> Capture "contract-instance-id" ContractInstanceId :> "stop"
+           :> Description "Stop."
+           :> Post '[JSON] ()
+      :<|> Capture "contract-instance-id" ContractInstanceId :> "cancel"
+           :> Description "Cancel order."
+           :> ReqBody '[JSON] CancelOrderParams :> Post '[JSON] ()
