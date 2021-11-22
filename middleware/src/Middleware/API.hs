566a77857af11f56b7a122f2d3684d7a9f015ca1
Tomasz Hołubowicz <tomasz.holubowicz@binarapps.com>
2021-11-22 16:11:07 +0100

    [PLTS-95] Clean up API

--- middleware/src/Middleware/API.hs
+++ middleware/src/Middleware/API.hs
@@ -10,45 +10,52 @@ import           Middleware.Dex.Types       (CancelOrderParams (CancelOrderParam
                                              PerformRandomParams (PerformRandomParams))
 import           Middleware.PabClient.Types (ContractInstanceId)
 import           Servant                    (Capture, Description, Get, JSON,
-                                             Post, ReqBody, (:<|>), (:>))
+                                             NoContent, Post, PostAccepted,
+                                             PostCreated, ReqBody, (:<|>), (:>))
 import           Servant.Swagger.UI         (SwaggerSchemaUI)
 
-type API = Capture "contract-instance-id" ContractInstanceId :> "funds"
-           :> Description "List of user funds."
-           :> Get '[JSON] [FundView]
-      :<|> Capture "contract-instance-id" ContractInstanceId :> "collect-funds"
-           :> Description "Collect user funds."
-           :> Post '[JSON] ()
-      :<|> Capture "contract-instance-id" ContractInstanceId :> "create-sell-order"
-           :> Description "Create a sell order."
-           :> ReqBody '[JSON] CreateSellOrderParams :> Post '[JSON] ()
-      :<|> Capture "contract-instance-id" ContractInstanceId :> "create-liquidity-pool"
-           :> Description "Create a liquidity pool"
-           :> ReqBody '[JSON] CreateLiquidityPoolParams :> Post '[JSON] ()
-      :<|> Capture "contract-instance-id" ContractInstanceId :> "create-liquidity-order"
-           :> Description "Create a liquidity order."
-           :> ReqBody '[JSON] CreateLiquidityOrderParams :> Post '[JSON] ()
-      :<|> Capture "contract-instance-id" ContractInstanceId :> "orders"
-           :> Description "List user orders."
-           :> Get '[JSON] [OrderView]
-      :<|> Capture "contract-instance-id" ContractInstanceId :> "all-orders"
-           :> Description "List all orders."
-           :> Get '[JSON] [OrderView]
-      :<|> Capture "contract-instance-id" ContractInstanceId :> "payouts"
-           :> Description "List user payouts."
-           :> Get '[JSON] [PayoutView]
-      :<|> Capture "contract-instance-id" ContractInstanceId :> "perform"
-           :> Description "Perform match."
-           :> Post '[JSON] ()
-      :<|> Capture "contract-instance-id" ContractInstanceId :> "perform-random"
-           :> Description "Perform random match."
-           :> ReqBody '[JSON] PerformRandomParams  :> Post '[JSON] ()
-      :<|> Capture "contract-instance-id" ContractInstanceId :> "stop"
-           :> Description "Stop."
-           :> Post '[JSON] ()
-      :<|> Capture "contract-instance-id" ContractInstanceId :> "cancel"
-           :> Description "Cancel order."
-           :> ReqBody '[JSON] CancelOrderParams :> Post '[JSON] ()
+type API =
+  Capture "contract-instance-id" ContractInstanceId :> "funds"
+    :> Description "List of user funds."
+    :> Get '[JSON] [FundView]
+    :<|> Capture "contract-instance-id" ContractInstanceId :> "collect-funds"
+      :> Description "Collect user funds."
+      :> PostAccepted '[JSON] NoContent
+    :<|> Capture "contract-instance-id" ContractInstanceId :> "create-sell-order"
+      :> Description "Create a sell order."
+      :> ReqBody '[JSON] CreateSellOrderParams
+      :> PostCreated '[JSON] NoContent
+    :<|> Capture "contract-instance-id" ContractInstanceId :> "create-liquidity-pool"
+      :> Description "Create a liquidity pool"
+      :> ReqBody '[JSON] CreateLiquidityPoolParams
+      :> PostCreated '[JSON] NoContent
+    :<|> Capture "contract-instance-id" ContractInstanceId :> "create-liquidity-order"
+      :> Description "Create a liquidity order."
+      :> ReqBody '[JSON] CreateLiquidityOrderParams
+      :> Post '[JSON] NoContent
+    :<|> Capture "contract-instance-id" ContractInstanceId :> "orders"
+      :> Description "List user orders."
+      :> Get '[JSON] [OrderView]
+    :<|> Capture "contract-instance-id" ContractInstanceId :> "all-orders"
+      :> Description "List all orders."
+      :> Get '[JSON] [OrderView]
+    :<|> Capture "contract-instance-id" ContractInstanceId :> "payouts"
+      :> Description "List user payouts."
+      :> Get '[JSON] [PayoutView]
+    :<|> Capture "contract-instance-id" ContractInstanceId :> "perform"
+      :> Description "Perform match."
+      :> PostAccepted '[JSON] NoContent
+    :<|> Capture "contract-instance-id" ContractInstanceId :> "perform-random"
+      :> Description "Perform random match."
+      :> ReqBody '[JSON] PerformRandomParams
+      :> PostAccepted '[JSON] NoContent
+    :<|> Capture "contract-instance-id" ContractInstanceId :> "stop"
+      :> Description "Stop."
+      :> PostAccepted '[JSON] NoContent
+    :<|> Capture "contract-instance-id" ContractInstanceId :> "cancel"
+      :> Description "Cancel order."
+      :> ReqBody '[JSON] CancelOrderParams
+      :> PostAccepted '[JSON] NoContent
 
 type SwaggerAPI = SwaggerSchemaUI "swagger-ui" "swagger.json"
 
